77208f321ffd65f6a77657363cf287fae46ba416
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-06 19:56:22 +0200

    Ajout demande du nom

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 class Personne:
-    def __init__(self, nom, age: int):
-        self.nom = nom
+    def __init__(self, nom: str = "", age: int = 0):
+        if nom == "":
+            self.nom = self.DemanderNom()
+        else:
+            self.nom = nom
         self.age = age
-        print("Constructeur personne " + nom)
+
+        print("Constructeur personne " + self.nom)
 
     def SePresenter(self):
-        print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
-        if self.EstMajeur():
-            print("Je suis majeur")
-        else:
-            print("Je suis mineur")
+        infos_personne = "Bonjour, je m'appelle " + self.nom
+        if self.age > 0:
+            infos_personne += ", j'ai " + str(self.age) + " ans"
+        print(infos_personne)
+        if self.age > 0:
+            if self.EstMajeur():
+                print("Je suis majeur")
+            else:
+                print("Je suis mineur")
 
     def EstMajeur(self):
         return self.age >= 18
 
+    def DemanderNom(self):
+        nom = input("Quel est votre nom ? ")
+        return nom
 
-personne1 = Personne("Jean", 30)
+
+personne1 = Personne("Jean", 0)
 personne2 = Personne("Paul", 15)
+personne3 = Personne("", 45)
 
 personne1.SePresenter()
 personne2.SePresenter()
+personne3.SePresenter()
