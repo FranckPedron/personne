ecf0011d18df2417ea0a1ce7b5d562f0d5c3411d
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-06 19:26:53 +0200

    Ajout test majorité

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 class Personne:
-    def __init__(self, nom, age):
+    def __init__(self, nom, age: int):
         self.nom = nom
         self.age = age
         print("Constructeur personne " + nom)
 
     def SePresenter(self):
-        print("Bonjour, je m'appelle " + self.nom + " et j'ai " + str(self.age) + " ans")
+        print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
+        if self.EstMajeur():
+            print("Je suis majeur")
+        else:
+            print("Je suis mineur")
+
+    def EstMajeur(self):
+        return self.age >= 18
 
 
-personne1 = Personne("Jean", 25)
-personne2 = Personne("Paul", 40)
+personne1 = Personne("Jean", 30)
+personne2 = Personne("Paul", 15)
 
 personne1.SePresenter()
 personne2.SePresenter()
